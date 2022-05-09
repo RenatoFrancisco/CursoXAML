532455435c77d1112224ac215ecb1e2d949881f4
RenatoFrancisco <renatofrancisco.s@gmail.com>
2022-05-09 14:06:19 -0300

    Set Properties with Property Element Syntax

--- src/WiredBrainCoffee.CustomersApp/MainPage.xaml.cs
+++ src/WiredBrainCoffee.CustomersApp/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,12 @@ namespace WiredBrainCoffee.CustomersApp
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void ButtonAddCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            var messageDialog = new MessageDialog("Customer added!");
+            await messageDialog.ShowAsync();
         }
     }
 }
