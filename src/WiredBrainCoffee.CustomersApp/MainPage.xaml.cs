3f04ff4bb7e51c6d596aae9748a1f6c95297d29d
RenatoFrancisco <renatofrancisco.s@gmail.com>
2022-05-11 21:44:55 -0300

    Create a Nested Grid for the Navigation

--- src/WiredBrainCoffee.CustomersApp/MainPage.xaml.cs
+++ src/WiredBrainCoffee.CustomersApp/MainPage.xaml.cs
@@ -33,5 +33,15 @@ namespace WiredBrainCoffee.CustomersApp
             var messageDialog = new MessageDialog("Customer added!");
             await messageDialog.ShowAsync();
         }
+
+        private void ButtonDeleteCustomer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonMove_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
