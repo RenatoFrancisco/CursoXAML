c96b925168809813122705208b6e6bf2e3e536cc
RenatoFrancisco <renatofrancisco.s@gmail.com>
2022-05-23 22:33:45 -0300

    Convert from String to a Customer in UWP

---
+++ src/WiredBrainCoffee.CustomersApp/Model/CustomerConverter.cs
@@ -0,0 +1,16 @@
+﻿namespace WiredBrainCoffee.CustomersApp.Model
+{
+    public static class CustomerConverter
+    {
+        public static Customer CreateCustomerFromString(string input)
+        {
+            var values = input.Split(',');
+            return new Customer
+            {
+                FirstName = values[0],
+                LastName = values[1],
+                IsDeveloper = bool.Parse(values[2]),
+            };
+        }
+    }
+}
